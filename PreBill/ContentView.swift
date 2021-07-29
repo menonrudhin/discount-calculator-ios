5a6eb4adc20ba95a9436e26aebbd5205ac400265
Rudhin Menon <menonrudhin@gmail.com>
2021-07-28 21:19:10 -0400

    initial commit

---
+++ PreBill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PreBill
+//
+//  Created by Rudhin Menon on 2021-07-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreBill/PreBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreBillApp.swift
+//  PreBill
+//
+//  Created by Rudhin Menon on 2021-07-28.
+//
+
+import SwiftUI
+
+@main
+struct PreBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
