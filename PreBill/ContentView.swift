dc52967c4a5660fd14d45317c53836e4bdd87496
Rudhin Menon <menonrudhin@gmail.com>
2021-07-31 19:54:03 -0400

    Calculate amount when price and discount value changes
    Enable decimalPad to enable user to enter only decimal numbers into the field

--- PreBill/ContentView.swift
+++ PreBill/ContentView.swift
@@ -16,14 +16,31 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Test App").foregroundColor(Color.white)
-              .padding().background(Color.green.blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)).frame(maxWidth: .infinity);
+              .padding().background(Color.green.blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/));
             
             HStack {
-                TextField("Price", text: $price).border(Color(UIColor.separator));
-                TextField("Dicount", text: $discount).border(Color(UIColor.separator));
-                TextField("Amount", text: $amount).border(Color(UIColor.separator));
+                TextField("Price", text: $price).border(Color(UIColor.separator)).keyboardType(.decimalPad)
+                    .onChange(of: price, perform: { value in
+                    calculate()
+                });
+                TextField("Dicount", text: $discount).border(Color(UIColor.separator)).keyboardType(.decimalPad).onChange(of: discount, perform: {value in calculate()
+                });
+                Text(amount).border(Color(UIColor.separator));
             }
         }
+    }
+    
+    /**
+            Calculate "amount" whenever user inputs change
+     */
+    func calculate() -> Void {
+        print("enter calculate")
+        let _price: Float = (price as NSString).floatValue; // "let" as the value is never mutated in this scope
+        let _discount: Float = (discount as NSString).floatValue;
+        var _amount: Float = 0.0; // "var" to enable mutation
+        _amount = _price - (_price * (_discount/100));
+        amount = String(_amount);
+        print("exit calculate")
     }
 }
 
--- PreBill/PreBillApp.swift
+++ PreBill/PreBillApp.swift
@@ -12,7 +12,6 @@ struct PreBillApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView();
-            SecondView();
         }
     }
 }
